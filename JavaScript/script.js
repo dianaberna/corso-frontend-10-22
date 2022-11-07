278b9451e9723470dae71d7abc88a7a3fd57f300
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2022-11-07 17:30:57 +0100

    review

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,7 +5,7 @@
 // Variabili var, let e const
 var a = 1; // var si utilizzava fino al 2015 che sono state inoltrate le variabili let e const
 let b = 2; // la grande differenza da var, che le variabili con let che dichiariamo all'interno di un blocco non si possono chiamare al di fuori del nostro blocco. Questo ci da dei vanataggi, uno è quello che su progetti molto grandi la nostra variabile non rimane salvata in memoria tutto il tempo quindi questo permette di avere dei programmi più leggeri rispetto al totale utilizzo di var.
-const c = '3'; // se siamo consapevoli che la nostra variabile non cambierà mai, allora per non avere errori non voluti impostiamo la nostra variabile con const così siamo consapevoli che non cambierà mai.
+const c = 3; // se siamo consapevoli che la nostra variabile non cambierà mai, allora per non avere errori non voluti impostiamo la nostra variabile con const così siamo consapevoli che non cambierà mai.
 console.log(typeof c);
 // CONDIZIONI
 // Se b è magiore di zero allora la mia console farà vedere b è maggiore di 0 altrimenti la console stamperà che b è minore di 0
