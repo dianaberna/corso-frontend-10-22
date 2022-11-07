293678d427ee31606a1a9206a906585ed4ee783a
SabakuNoMaki <veronicarsp@gmail.com>
2022-11-07 16:27:04 +0100

    modifica cartelle

---
+++ JavaScript/script.js
@@ -0,0 +1,26 @@
+/* commento su più righe */
+// commebnto su una riga
+
+console.log("ciao mondo");
+
+var primavariabile = 1;
+var secondavariabile = 2;
+console.log(primavariabile);
+console.log(secondavariabile);
+
+var  primastringa = "prima stringa";
+console.log(primastringa);
+
+let nuovavariabile = 3;
+console.log(nuovavariabile);
+nuovavariabile = 13;
+console.log(nuovavariabile);
+
+const primacostante = 4;
+console.log(primacostante);
+
+let numerostringa1 = "1";
+let numerostringa2 = "2";
+let nuovasomma = numerostringa1 + numerostringa2;
+console.log(nuovasomma);
+console.log(typeof nuovasomma);
