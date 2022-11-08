21971c00174fbd74ffbe420a2e5d938f4bb96eb1
dianaberna <bernabei.d@gmail.com>
2022-11-08 12:52:22 +0100

    aggiornato file array.js

--- js/array.js
+++ js/array.js
@@ -1,13 +1,25 @@
 // array
 let animali = ["gatto", "cane", "coniglio"];
-console.log(animali)
-console.log(animali.length)
+/* console.log(animali)
+console.log(animali.length) */
 
-let arraymisto = ["gatto", 2, 5, "topo", 3, 299]
-console.log(arraymisto)
-console.log(arraymisto.length)
+let arraymisto = ["gatto", 2, 5, "topo", 3, 299];
+// "gatto" è in posizione/indice 0
+// 2 è in posizione 1
+// 5 è in posizione 2
+// "topo" è in posizione 3
+console.log(arraymisto);
+console.log(arraymisto.length); // stampa 6
+// pippo[a] --> l'elemento dell'array pippo in posizione a
 
-let arraynumerico = [3, 5, 10, 40]
+let a = 2;
+let b = 6;
+let c = 3;
+let d = 10;
+// se ne ho 100?
+let arraynumericovariabili = [a, b, c, d]; // array di variabili
+
+let arraynumerico = [2, 6, 3, 10];
 /*
     3 è in posizione 0 -> arraynumerico[0] = 3
     5 è in posizione 1 -> arraynumerico[1] = 5
@@ -16,8 +28,108 @@ let arraynumerico = [3, 5, 10, 40]
 
     la lunghezza dell'array è 4
 */
-console.log(arraynumerico)
-console.log(arraynumerico.length)
+console.log(arraynumerico);
+console.log("---");
 
-// sommare tutti gli elementi dell'array arraynumerico 
-let i = 0;
+// sommare tutti gli elementi dell'array arraynumerico
+let somma =
+    arraynumerico[0] + arraynumerico[1] + arraynumerico[2] + arraynumerico[3]; // ... per 100 elementi
+console.log(somma);
+console.log(arraynumerico.length); // lunghezza - 1
+
+console.log("---");
+let i = 0; // variabile che ci aiuterà a scorrere l'array
+console.log("stampo gli elementi con il while");
+while (i < arraynumerico.length) {
+    // finchè i non raggiunge la lunghezza dell'array continua a stampare
+    console.log("i = " + i);
+    console.log("arraynumerico [" + i + "] =" + arraynumerico[i]);
+    i = i + 1;
+}
+
+console.log("stampo gli elementi con il for");
+for (let pippo = 0; pippo < arraynumerico.length; pippo = pippo + 1) {
+    // non è eseguito all'istante
+    console.log("pippo = " + pippo);
+    console.log("arraynumerico [" + pippo + "] =" + arraynumerico[pippo]);
+}
+
+console.log("---");
+// let arraynumerico = [2, 6, 3, 10]
+console.log("stampo la somma degli elementi");
+let indice = 0;
+let sommabella = 0;
+while (indice < 4) {
+    console.log("indice = " + indice);
+    sommabella = sommabella + arraynumerico[indice]; //elemento in posizione indice nell'array arraynumerico
+    console.log("sommabella = " + sommabella);
+    indice = indice + 1;
+}
+console.log("sommabella fine while = " + sommabella);
+
+console.log("prova");
+indice = 0;
+sommabella = 0;
+while (indice < 4) {
+    console.log("indice = " + indice);
+    sommabella = sommabella + arraynumerico[3]; //elemento in posizione indice nell'array arraynumerico
+    console.log("sommabella = " + sommabella);
+    indice = indice + 1;
+}
+console.log("sommabella prova= " + sommabella);
+
+/*
+
+i = 0, sommabella = 0
+i < 4? 0 < 4? si, sommabella = 0 + arraynumerico[0] = 0 + 2 = 2, i = i + 1 = 0 + 1 = 1
+i < 4? 1 < 4? si, sommabella = 2 + arraunumerico[1] = 2 + 6 = 8, i = i + 1 = 1 + 1 = 2
+i < 4? 2 < 4? si, sommabella = 8 + arraynumerico[2] = 8 + 3 = 11, i = i + 1 = 2 + 1 = 3
+i < 4? 3 < 4? si, sommabella = 11 + arraynumerico[3] = 11 + 10 = 21, i = i + 1 = 3 + 1 = 4
+i < 4? 4 < 4? no
+esco dal while e stampo sommabella
+
+i = 0, sommabella = 0
+i > 4? 0 > 4? no
+esco dal while e stampo sommabella
+
+*/
+
+console.log("---");
+arraynumerico = [-5, 3, 2, -1, -6];
+// somma di tutti gli elementi positivi (>0) dell'array
+console.log("stampo la somma degli elementi positivi");
+indice = 0;
+sommapositivi = 0;
+while (indice < arraynumerico.length) {
+    console.log("indice = " + indice);
+    if (arraynumerico[indice] > 0) {
+        sommapositivi = sommapositivi + arraynumerico[indice];
+    }
+    console.log("sommapositivi = " + sommapositivi);
+    indice = indice + 1;
+}
+console.log("sommapositivi fine while = " + sommapositivi);
+
+console.log("---");
+
+// somma di tutti gli elementi con indice/posizione pari
+/*
+arraynumerico = [-5, 3, 2, -1, -6, 1]
+    posizioni   [ 0, 1, 2,  3,  4, 5]
+
+sommo 2 e -6? somma di tutti i numeri pari
+sommo -5, 2, -6? somma di tutti gli elementi in posizione pari
+*/
+
+console.log("stampo la somma degli elementi in posizione pari");
+indice = 0;
+sommaposizionepari = 0;
+while (indice < arraynumerico.length) {
+    console.log("indice = " + indice);
+    if (indice % 2 == 0) {
+        sommaposizionepari = sommaposizionepari + arraynumerico[indice];
+    }
+    console.log("sommaposizionepari = " + sommaposizionepari);
+    indice = indice + 1;
+}
+console.log("sommaposizionepari fine while = " + sommaposizionepari);
