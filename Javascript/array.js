593d10d6094076eb2e270871d37b09419df6a271
“Luca1791” <demagluca1791@gmail.com>
2022-11-08 12:00:52 +0100

    Array

---
+++ Javascript/array.js
@@ -0,0 +1,15 @@
+let linguaggi = ["HTML","CSS","Javascript","Python","PHP"]
+// let mix = ["Tizio","Caio",32,22,[10,20]] 
+// console.log(mix[1]) // Caio
+// console.log(mix[4][1]) // 20
+console.log(linguaggi.length + " " + "elementi contiene l'array linguaggi");
+
+let mixNumeri = [10, 17,30]
+mixNumeri.push(3)   // aggiunge un numero alla fine di un array
+console.log( mixNumeri); 
+
+
+
+let numeri = [40, 50, 60]
+numeri.pop()   // toglie un numero alla fine di un array
+console.log( numeri); 
