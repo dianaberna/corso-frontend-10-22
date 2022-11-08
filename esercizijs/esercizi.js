25cc6d35174f1d8de94da5741a37e6cc9dafc94c
“Diesis92” <cristina199280@gmail.com>
2022-11-08 18:02:28 +0200

    esercizi

--- esercizijs/esercizi.js
+++ esercizijs/esercizi.js
@@ -2,4 +2,27 @@
 let array =[ 2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
 console.log(array);
 console.log(array.length);
+let i = 0
+while (i<array.length) {
+  console.log (array[i]);
+  i= i+1;  
+}
 
+let somma = 0
+i = 0
+while (i<array.length ) {
+    somma = somma + array[i];
+    i= i+1;
+}
+
+console.log (somma);
+
+let sommadispari = 0
+i= 0
+while (i<array.length ) {
+    if (array[i]%2!=0) {
+        sommadispari = sommadispari + array [i]
+    }
+    i= i+1;
+}
+console.log (sommadispari);
