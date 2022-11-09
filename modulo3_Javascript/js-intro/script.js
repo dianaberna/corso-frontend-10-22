2f72ea9ec2dfefca6c31af1dbc502ec137855130
Claudia Piccioni <mail@claudiapiccioni.it>
2022-11-09 16:40:00 +0100

    corretti esercizi

--- modulo3_Javascript/js-intro/script.js
+++ modulo3_Javascript/js-intro/script.js
@@ -1,7 +1,7 @@
 mioArray = [2, 6, 9, 10, -2, -3, 0, 2, 5, 1];
 console.log(mioArray);
 
-//ok
+//OK Stampare la somma degli elementi in posizione pari
 console.log("00. Stampare la somma degli elementi in posizione pari");
 i = 0;
 sommaPosizionePari = 0;
@@ -16,7 +16,7 @@ while (i < mioArray.length) {
 
 
 
-//ok
+//OK Stampare tutti gli elementi dell'array
 console.log("01. Stampare tutti gli elementi dell'array");
 
 for (i = 0; i < mioArray.length; i++) {
@@ -24,9 +24,7 @@ for (i = 0; i < mioArray.length; i++) {
 }
 
 
-
-
-//ok
+//OK Stampare la somma di tutti gli elementi dell'array
 console.log("02. Stampare la somma di tutti gli elementi dell'array");
 
 sommaElementi = 0;
@@ -37,9 +35,8 @@ console.log(sommaElementi);
 
 
 
-
-
-//ok
+// OK Stampare la somma dei soli elementi dispari
+// Stampare la somma dei soli elementi dispari
 console.log("03. Stampare la somma dei soli elementi dispari");
 
 let sommaDispari = 0;
@@ -52,11 +49,8 @@ for (i = 0; i < mioArray.length; i++) {
 console.log("la somma dei numeri dispari è " + sommaDispari);
 
 
-
-
-
-
-//ok
+// Stampare quante volte compare l'elemento 2
+// OK
 console.log("04. Stampare quante volte compare l'elemento 2");
 
 quantiDue = 0;
@@ -69,10 +63,8 @@ console.log("il numero 2 compare " + quantiDue + " volte");
 
 
 
-
-
-
-//ok
+// OK
+// Stampare quanti numeri positivi (compreso lo zero) ci sono nell'array
 console.log("05. Stampare quanti numeri positivi (compreso lo zero) ci sono nell'array");
 
 numeriPositivi = 0;
@@ -87,7 +79,8 @@ console.log("i numeri positivi sono " + numeriPositivi);
 
 
 
-//ok
+// OK
+// Stampare il numero massimo (con FOR)
 console.log("07. Stampare il numero massimo");
 
 numeroMassimo = 0;
@@ -102,11 +95,10 @@ for (i = 0; i < mioArray.length; i++) {
 console.log("il numero massimo è " + numeroMassimo);
 
 
-
-
-
-//ok (usando però il while)
+// OK
+// Stampare il numero minimo (con WHILE)
 console.log("08. Stampare il numero minimo");
+
 numeroMinimo = 0;
 i = 0
 while (i < mioArray.length) {
@@ -121,9 +113,8 @@ console.log("il numero minimo è " + numeroMinimo);
 
 
 
-
-
-//ok senza push
+// OK senza push
+// Inserire in un nuovo array solo gli elementi negativi
 console.log("09/a. Inserire in un nuovo array solo gli elementi negativi");
 
 arrayConNegativi = [];
@@ -137,8 +128,8 @@ for (i = 0; i < mioArray.length; i++) {
 console.log(arrayConNegativi);
 
 
-
-//ok usando push
+// OK usando PUSH (e FOR)
+// Inserire in un nuovo array solo gli elementi negativi
 console.log("09/b. Inserire in un nuovo array solo gli elementi negativi");
 
 numNegativi = [];
@@ -150,7 +141,8 @@ for (i = 0; i < mioArray.length; i++) {
 console.log(numNegativi);
 
 
-//usando push e while
+// OK usando PUSH (e WHILE)
+// Inserire in un nuovo array solo gli elementi negativi
 console.log("09 /b.Inserire in un nuovo array solo gli elementi negativi");
 
 numNegativi = [];
@@ -164,9 +156,9 @@ while (i < mioArray.length) {
 console.log(numNegativi);
 
 
-
-//ok
-console.log("10/a. calcola il doppio di ogni elemento");
+// ok ma NON un array
+// calcola il doppio di ogni elemento
+console.log("10/a. Calcola il doppio di ogni elemento");
 
 doppio = 0;
 for (i = 0; i < mioArray.length; i++) {
@@ -174,7 +166,9 @@ for (i = 0; i < mioArray.length; i++) {
   console.log("il doppio di " + mioArray[i] + " è " + doppio);
 }
 
-//ok? senza push
+
+// OK senza push, senza variabile nuovo indice (tanto è "OGNI" valore dell'array)
+// Creare un nuovo array dove ogni elemento del nuovo è uguale al doppio di quello dato
 console.log("10/b. Creare un nuovo array dove ogni elemento del nuovo è uguale al doppio di quello dato");
 
 arrayDoppio = [];
@@ -186,38 +180,39 @@ for (i = 0; i < mioArray.length; i++) {
 console.log(arrayDoppio);
 
 
-//ok senza push con variabile nuovo indice
+
+// OK senza push, con variabile NUOVO INDICE
+// Creare un nuovo array dove ogni elemento del nuovo è uguale al doppio di quello dato
 console.log("10/c. Creare un nuovo array dove ogni elemento del nuovo è uguale al doppio di quello dato");
 
-arrayConDoppi = [];
+arrayDoppi = [];
 indiceNuovoArray = 0;
 for (i = 0; i < mioArray.length; i++) {
-  arrayConDoppi[indiceNuovoArray] = 2 * mioArray[i];
+  arrayDoppi[indiceNuovoArray] = mioArray[i] * 2;
   indiceNuovoArray++;
 };
-console.log(arrayConDoppi);
+console.log(arrayDoppi);
 
 
 
 
-
-
-
-//no
+//
+// Creare un nuovo array in cui inserisco due volte (una di seguito all'altra) l'array dato
 console.log("11. Creare un nuovo array in cui inserisco due volte (una di seguito all'altra) l'array dato");
 
-doppioArray = [];
-nuovoIndice = 0;
 i = 0;
+let arrayDuevolte = [];
+while (i < mioArray.length) {
+  arrayDuevolte[i] = mioArray[i]; // stessi elementi
+  arrayDuevolte[i + mioArray.length] = mioArray[i]; // aggiungo 10 alla numerazione dell'indice per gli ulteriori elementi.
+  i++;
+}
+console.log(arrayDuevolte);
 
-doppioArray = mioArray.push(mioArray);
-
-console.log(doppioArray);
-console.log(mioArray);
 
 
-
-// 12a ok - ma non in un array
+// ok
+// Stampare l'array con gli elementi al contrario - elenco NON in un array
 console.log("12a. Stampare al contrario gli elementi dell'array:[1, 5, 2, 0, -3, -2, 10, 9, 6, 2]");
 
 mioArray = [1, 5, 2, 0, -3, -2, 10, 9, 6, 2];
@@ -231,7 +226,7 @@ console.log(alContrario);
 
 
 
-// 12b OK
+// OK
 // Stampare l'array con gli elementi al contrario usando: (i < array)
 
 console.log("12b. Stampare l'array con gli elementi al contrario");
@@ -242,7 +237,7 @@ while (i < mioArray.length) {
 console.log(mioArray);
 
 
-// 12c OK
+// OK
 // Stampare l'array con gli elementi al contrario usando: (i >= 0)
 
 console.log("12c. Stampare l'array con gli elementi al contrario");
@@ -271,34 +266,132 @@ console.log("Ci sono " + conteggio + " parole di quattro lettere");
 
 
 
-//quanto è lungo il nostro array?
-// console.log(indice);
+// 13. Versione Diana
+// Creare un array3 con la somma degli elementi dell'array1 e dell'array2 [5, 4, 4, 7, 4]
+let array1 = [1, 2, 2, 3, 4];
+let array2 = [4, 2, 2, 4];
 
+console.log("13) Creare un array3 con la somma degli elementi dell'array1 e dell'array2 [5, 4, 4, 7, 4]");
 
+i = 0;
+let indicearray3 = 0;
+let array3 = [];
 
-// array1 = [1, 2, 2, 3, 4];
-// array2 = [4, 2, 2, 4];
-
-// console.log("13) Creare un array3 con la somma degli elementi dell'array1 e dell'array2 [5, 4, 4, 7, 4]");
-
-// indiceNuovoArray = 0;
-// nuovoArray = [];
-// for (i = 0; i < array1.lentgh; i++);
-
-
-
-
-
-// console.log("14) Creare un array3 con gli elementi (non la posizione) dispari di array1 e gli elementi pari di array2: versione1 [1,3,4,2,2,4] 	versione2 [1,4,2,2,3,4]");
-
-// console.log("15) Aggiornare l'array2 con elemento = il suo valore diviso il valore nella posizione lunghezza-posizione dell'array1");
-
-// console.log("16) Verificare se gli array sono palindromi come ad esempio anna, radar, emme");
-
-
-
+while (i < array1.length || i < array2.length) {
+  array3[i] = 0;
+  if (array1[i]) {
+    array3[i] = array3[i] + array1[i];
+  }
+  if (array2[i]) {
+    array3[i] = array3[i] + array2[i];
+  }
+  i++;
+}
+console.log(array3);
 
 
 
 
 
+// 14. versione Diana
+// Creare un array3 con gli elementi (non la posizione) dispari di array1 e gli elementi pari di array2
+console.log("14) Creare un array3 con gli elementi(non la posizione) dispari di array1 e gli elementi pari di array2: versione1 [1,3,4,2,2,4] 	versione2 [1,4,2,2,3,4]");
+
+array1 = [1, 2, 2];
+array2 = [4, 2, 2, 4];
+i = 0;
+array3 = [];
+indicearray3 = 0;
+// versione 1
+while (i < array1.length) {
+  if (array1[i] % 2 == 1) {
+    array3[indicearray3] = array1[i];
+    indicearray3++;
+  }
+  i++;
+}
+i = 0;
+while (i < array2.length) {
+  if (array2[i] % 2 == 0) {
+    array3[indicearray3] = array2[i];
+    indicearray3++;
+  }
+  i++;
+}
+console.log(array3); //[1,3,4,2,2,4]
+
+//versione 2
+i = 0;
+array3 = [];
+indicearray3 = 0;
+while (i < array1.length || i < array2.length) {
+  if (array1[i] % 2 == 1) {
+    array3[indicearray3] = array1[i];
+    indicearray3++;
+  }
+  if (array2[i] % 2 == 0) {
+    array3[indicearray3] = array2[i];
+    indicearray3++;
+  }
+  i++;
+}
+
+console.log(array3); //[1,4,2,2,3,4]
+
+
+
+
+
+// 15. Versione Diana
+// Aggiornare l'array2 con elemento = il suo valore diviso il valore nella posizione lunghezza-posizione dell'array1
+console.log("15) Aggiornare l'array2 con elemento = il suo valore diviso il valore nella posizione lunghezza-posizione dell'array1");
+
+console.log("-- 15 --");
+array1 = [1, 2, 2, 3, 4];
+array2 = [4, 2, 2, 4];
+i = 0;
+while (i < array2.length) {
+  if (array1.length >= array2.length) {
+    array2[i] = array2[i] / array1[array2.length - i];
+  }
+
+    // 0 -> 4 / array1[4 - 0]
+    // 1 -> 2 / array1[4 - 1]
+    // 2 -> 2 / array1[4 - 2]
+    // 3 -> 4 / array1[4 - 3]
+
+  console.log(array2[i]);
+  i++;
+}
+
+
+// Versione Diana
+// Verificare se gli array sono palindromi come ad esempio anna, radar, emme
+console.log("16) Verificare se gli array sono palindromi come ad esempio anna, radar, emme");
+
+// 16 - Verificare se gli array sono palindromi come ad esempio “anna”, “radar”, “emme”
+console.log("-- 16 --");
+array2 = [4, 2, 2, 4];
+// array2 = [4, 2, 3, 2, 4];
+i = 0;
+cont = 0;
+// array.lenght = 4 / 2
+// i = 0 -> 4 == 4
+// i = 1 -> 2 == 2
+// array1 = [4, 2, 3, 2, 4]
+// array.lenght = 5 / 2 = 2
+// i = 0 -> 4 == 4 cont = 1
+// i = 1 -> 2 == 2 cont = 2
+while (i < Math.floor(array2.length) / 2) {
+  // se lunghezza è pari
+  if (array2[i] === array2[array2.length - i - 1]) {
+    cont++;
+  }
+  i++;
+}
+
+if (cont == array2.length / 2 || cont == Math.floor(array2.length / 2) + 1) {
+  console.log("è palindromo");
+} else {
+  console.log("non è palindromo");
+}
