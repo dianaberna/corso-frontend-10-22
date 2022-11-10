d44970080193cbe25198055f7d15b763ddded183
dianaberna <bernabei.d@gmail.com>
2022-11-10 12:13:31 +0100

    aggiornato string.js

--- js/pt1/base/string.js
+++ js/pt1/base/string.js
@@ -8,8 +8,8 @@ console.log(str.repeat(3))
 console.log(str.slice(6))
 console.log(str.split(" "))
 
-let nuovastringa = "hello.world.ciao.ciao"
-console.log(nuovastringa.split("."))
+let stringaprova = "hello.world.ciao.ciao"
+console.log(stringaprova.split("."))
 
 console.log(str.substring(1,5))
 console.log(str.indexOf("World"))
@@ -17,11 +17,11 @@ console.log(str.indexOf("World"))
 console.log(str.includes("ciao"))
 console.log(str.includes("prova"))
 
-let primastringa = "ciao"
-let secondastringa = "mondo"
-console.log(primastringa+secondastringa)
-console.log(primastringa.concat(secondastringa))
-let i = 0;
+let stringa1 = "ciao"
+let stringa2 = "mondo"
+console.log(stringa1+stringa2)
+console.log(stringa1.concat(stringa2))
+let indice = 0;
 console.log("i= "+i)
-console.log(`ciao ciao ciao ${i} `);
-console.log("ciao ciao ciao "+i+" ");
+console.log(`ciao ciao ciao ${indice} `);
+console.log("ciao ciao ciao "+indice+" ");
