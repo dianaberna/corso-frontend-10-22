af2b8ee108b5f0d44cc022b99926fe06235a750d
Danilo89 <danilocarretta@gmail.com>
2022-11-10 12:12:29 +0100

    aggiorno esercizi funzioni

--- javascript mio/eserciziFunzioni.js
+++ javascript mio/eserciziFunzioni.js
@@ -1,15 +1,53 @@
 // Moltiplicazione di due numeri
 
-function moltiplicazioneDuenumeri(primonumero, secondonumero) {
+console.log("Moltiplicazione tra due numeri");
+function moltiplicazioneDueNumeri(primonumero, secondonumero) {
     let m = primonumero * secondonumero;
 
     return m;
-} // fine function moltiplicazione
+} // fine moltiplicazioneDuenumeri
 
-molt = moltiplicazioneDuenumeri(4, 8);
+molt = moltiplicazioneDueNumeri(4, 8);
 
-console.log(moltiplicazioneDuenumeri(5, 6));
+console.log(moltiplicazioneDueNumeri(5, 6));
 console.log(molt);
 
+console.log("\n");
+
 // Convertire anni in giorni
+
+console.log("Convertire anni in giorni");
+function convertitoreAnniGiorni(anni) {
+    let g = anni * 365;
+
+    return g;
+} // fine convertitoreAnnigiorni
+
+giorni = convertitoreAnniGiorni(5);
+
+console.log(giorni);
+console.log(convertitoreAnniGiorni(1));
+
+console.log("\n");
+
+// Minore o uguale a zero
+
+console.log("Minore o uguale a zero");
+function minoreUgualeAZero(numero) {
+    if (numero <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let rest = minoreUgualeAZero(-4);
+
+console.log(minoreUgualeAZero(6));
+console.log(rest);
+
+console.log("\n");
+
+// Il problema della fattoria
+
 
