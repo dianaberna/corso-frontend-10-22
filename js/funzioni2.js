930448d561fa926b34bbc70f185ee57746c00238
“Diesis92” <cristina199280@gmail.com>
2022-11-10 12:12:56 +0200

    esercizijs

---
+++ js/funzioni2.js
@@ -0,0 +1,10 @@
+//Crea una funzione che accetta due numeri come argomenti 
+//e ne restituisce la moltiplicazione.
+//moltiplicazione(3, 2) ➞ 6
+
+
+function moltiplicazione(primonumero, secondonumero) {
+    let a = primonumero;
+    let b = secondonumero;
+    return a,b [3*2];
+    }
