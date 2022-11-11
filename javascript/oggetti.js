9a938ded06faadd7311b1ef1743b0deba7aa47e4
fabiobillo <billerifabio@gmail.com>
2022-11-11 12:52:29 +0100

    completamento esercizi

--- javascript/oggetti.js
+++ javascript/oggetti.js
@@ -1,4 +1,4 @@
-
+console.log("budgetTotle");
   function budgetTotale(oggetto){
     let budgetTot=0
     let i=0
@@ -23,5 +23,45 @@ console.log(
         { name: "John", age: 21, budget: 29000 },
         { name: "Steve", age: 32, budget: 32000 },
         { name: "Martin", age: 16, budget: 1600 },
+    ])
+);
+
+
+console.log("PrezzoTotaleAlimenti");
+
+function prezzoTotaleAlimenti(oggetto){
+    let spesaTot=0
+    let i=0
+    for (i=0; i<oggetto.length; i++){
+        spesaTot+= oggetto[i].price
+        
+    }
+    return spesaTot.toFixed(2)
+  }
+  
+  console.log(
+    prezzoTotaleAlimenti([{ product: "milk", quantity: 1, price: 1.5 }])
+);
+console.log(
+    prezzoTotaleAlimenti([
+        { product: "milk", quantity: 1, price: 1.5 },
+        { product: "cereals", quantity: 1, price: 2.5 },
+    ])
+);
+console.log(
+    prezzoTotaleAlimenti([{ product: "milk", quantity: 3, price: 1.5 }])
+);
+console.log(
+    prezzoTotaleAlimenti([
+        { product: "milk", quantity: 1, price: 1.5 },
+        { product: "eggs", quantity: 12, price: 0.10 },
+        { product: "bread", quantity: 2, price: 1.6 },
+        { product: "cheese", quantity: 1, price: 4.5 },
+    ])
+);
+console.log(
+    prezzoTotaleAlimenti([
+        { product: "chocolate", quantity: 1, price: 0.10 },
+        { product: "lollipop", quantity: 1, price: 0.20 },
     ])
 );
