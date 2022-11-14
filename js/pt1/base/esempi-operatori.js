56662be91e78753b9faea3d1ddbd20537fc8791d
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2022-11-14 09:39:22 +0100

    merge main in my branch

---
+++ js/pt1/base/esempi-operatori.js
@@ -0,0 +1,24 @@
+// esempio calcolatrice
+let operatore = "+";
+let primonumero = 1;
+let secondonumero = 4;
+let appoggio = 0 
+switch (operatore){
+    case "+": 
+        appoggio = primonumero + secondonumero
+        break;
+    case "-": 
+        appoggio = primonumero - secondonumero
+        break;
+    case "*":
+        appoggio = primonumero * secondonumero
+        break;
+    case "/":
+        if(secondonumero != 0){
+            appoggio = primonumero / secondonumero
+        }else{
+            console.log("non posso dividere per zero")
+        }
+        break;
+}
+console.log(appoggio)
