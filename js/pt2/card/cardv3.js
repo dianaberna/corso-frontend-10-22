1f8df60f71c74b08cd584d112863f22235721650
dianaberna <bernabei.d@gmail.com>
2022-11-14 18:15:08 +0100

    aggiunta card v1

--- js/pt2/card/cardv3.js
+++ js/pt2/card/cardv3.js
@@ -1,126 +1,106 @@
 console.log("card.js")
 
-/*
-            <div class="card-body">
-                <img src="caffe.jpg" alt="caffe">
-                <div>
-                    <h1>Vanilla Latte</h1>
-                    <p>21K</p>
-                </div>
-            </div>
-        */
+// crea elemento con del contenuto testuale al suo interno
+function creaElemento(
+    tipo,
+    contenuto,
+    classe,
+    posizione,
+    attributi
+) {
+    let nuovoElemento = document.createElement(tipo);
+    if (attributi.src && attributi.alt) {
+        nuovoElemento.src = attributi.src;
+        nuovoElemento.alt = attributi.alt;
+    } else {
+        let contenutoElemento = document.createTextNode(contenuto);
+        nuovoElemento.appendChild(contenutoElemento);
+    }
+    if (classe != "") nuovoElemento.className = classe;
+    posizione.appendChild(nuovoElemento);
+}
 
-            // crea elemento con del contenuto testuale al suo interno
-            function creaElemento(
-                tipo,
-                contenuto,
-                classe,
-                posizione,
-                attributi
-            ) {
-                let nuovoElemento = document.createElement(tipo);
-                if (attributi.src && attributi.alt) {
-                    nuovoElemento.src = attributi.src;
-                    nuovoElemento.alt = attributi.alt;
-                } else {
-                    let contenutoElemento = document.createTextNode(contenuto);
-                    nuovoElemento.appendChild(contenutoElemento);
-                }
-                if (classe != "") nuovoElemento.className = classe;
-                posizione.appendChild(nuovoElemento);
-            }
+// creare un elemento che restituisce esclusivamente la creazione
+function creaNodo(tipo, classe) {
+    let nuovoElemento = document.createElement(tipo);
+    nuovoElemento.className = classe;
+    return nuovoElemento;
+}
 
-            // creare un elemento che restituisce esclusivamente la creazione
-            function creaNodo(tipo, classe) {
-                let nuovoElemento = document.createElement(tipo);
-                nuovoElemento.className = classe;
-                return nuovoElemento;
-            }
+let arrayCard = [
+    {
+        cover: {
+            src: "caffe.jpg",
+            alt: "caffè",
+        },
+        title: "Vanilla Latte",
+        price: "21K",
+        etichette: ["Hot", "Cold"],
+        buttonTitle: "Aggiungi",
+    },
+    {
+        cover: {
+            src: "caffe.jpg",
+            alt: "caffè",
+        },
+        title: "Cappuccino",
+        price: "10K",
+        etichette: ["Hot", "Cold", "Altro"],
+        buttonTitle: "Aggiungi",
+    },
+    {
+        cover: {
+            src: "caffe.jpg",
+            alt: "caffè",
+        },
+        title: "Caffè",
+        price: "1K",
+        etichette: ["Hot"],
+        buttonTitle: "Aggiungi",
+    },
+];
 
-            let arrayCard = [
-                {
-                    cover: {
-                        src: "caffe.jpg",
-                        alt: "caffè",
-                    },
-                    title: "Vanilla Latte",
-                    price: "21K",
-                    etichette: ["Hot", "Cold"],
-                    buttonTitle: "Aggiungi",
-                },
-                {
-                    cover: {
-                        src: "caffe.jpg",
-                        alt: "caffè",
-                    },
-                    title: "Cappuccino",
-                    price: "10K",
-                    etichette: ["Hot", "Cold", "Altro"],
-                    buttonTitle: "Aggiungi",
-                },
-                {
-                    cover: {
-                        src: "caffe.jpg",
-                        alt: "caffè",
-                    },
-                    title: "Caffè",
-                    price: "1K",
-                    etichette: ["Hot"],
-                    buttonTitle: "Aggiungi",
-                },
-            ];
+for (let i = 0; i < arrayCard.length; i++) {
+    let card = creaNodo("div", "card");
 
-            for (let i = 0; i < arrayCard.length; i++) {
-                let card = creaNodo("div", "card");
+    let cardBody = creaNodo("div", "card-body");
+    // cardBody.src="caffe.jpg" -> un div non può avere attributo src
 
-                let cardBody = creaNodo("div", "card-body");
-                // cardBody.src="caffe.jpg" -> un div non può avere attributo src
+    creaElemento("img", "", "", cardBody, arrayCard[i].cover);
 
-                creaElemento("img", "", "", cardBody, arrayCard[i].cover);
+    let nuovodivbody = document.createElement("div");
 
-                let nuovodivbody = document.createElement("div");
+    creaElemento("h1", arrayCard[i].title, "", nuovodivbody, "");
+    creaElemento("p", arrayCard[i].price, "", nuovodivbody, "");
+    cardBody.appendChild(nuovodivbody);
 
-                creaElemento("h1", arrayCard[i].title, "", nuovodivbody, "");
-                creaElemento("p", arrayCard[i].price, "", nuovodivbody, "");
-                cardBody.appendChild(nuovodivbody);
+    let divCardFooter = creaNodo("div", "card-footer");
+    let divEtichette = creaNodo("div", "etichette");
 
-                /*
-            <div class="card-footer">
-                <div class="etichette">
-                    <span>Hot</span>
-                    <span>Cold</span>
-                </div>
-                <button>Aggiungi</button>
-            </div>
-            */
+    // creare i due span
+    for (let j = 0; j < arrayCard[i].etichette.length; j++) {
+        creaElemento(
+            "span",
+            arrayCard[i].etichette[j],
+            "",
+            divEtichette,
+            ""
+        );
+    }
 
-                let divCardFooter = creaNodo("div", "card-footer");
-                let divEtichette = creaNodo("div", "etichette");
+    divCardFooter.appendChild(divEtichette);
 
-                // creare i due span
-                for (let j = 0; j < arrayCard[i].etichette.length; j++) {
-                    creaElemento(
-                        "span",
-                        arrayCard[i].etichette[j],
-                        "",
-                        divEtichette,
-                        ""
-                    );
-                }
+    // creare il bottone
+    creaElemento(
+        "button",
+        arrayCard[i].buttonTitle,
+        "",
+        divCardFooter,
+        ""
+    );
 
-                divCardFooter.appendChild(divEtichette);
+    card.appendChild(cardBody);
+    card.appendChild(divCardFooter);
 
-                // creare il bottone
-                creaElemento(
-                    "button",
-                    arrayCard[i].buttonTitle,
-                    "",
-                    divCardFooter,
-                    ""
-                );
-
-                card.appendChild(cardBody);
-                card.appendChild(divCardFooter);
-
-                document.body.appendChild(card);
-            }
+    document.body.appendChild(card);
+}
