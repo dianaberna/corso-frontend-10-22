8d38321b4bfa535f869be0cc0fc32b04ceefef82
Danilo89 <danilocarretta@gmail.com>
2022-11-17 09:59:09 +0100

    aggiungo file card.gtml, card.css, card.js nella cartella Esercizi DOM

---
+++ esercizi javascript/esercizi DOM/card.js
@@ -0,0 +1,68 @@
+let divCard = document.createElement("div");
+
+divCard.className = "card";
+document.body.appendChild(divCard);
+
+let divCardBody = document.createElement("div");
+
+divCardBody.className = "card-body";
+divCard.appendChild(divCardBody);
+
+let nuovaImmagine = document.createElement("img");
+
+nuovaImmagine.src = "caffe.jpg";
+nuovaImmagine.alt = "caffe";
+divCardBody.appendChild(nuovaImmagine);
+
+let divTitoli = document.createElement("div");
+
+let titolo = document.createElement("h1");
+
+let testoTitolo = document.createTextNode("Vanilla Latte");
+
+titolo.appendChild(testoTitolo);
+divTitoli.appendChild(titolo);
+divCardBody.appendChild(divTitoli);
+
+let paragrafo = document.createElement("p");
+let testoParagrafo = document.createTextNode("21K");
+
+paragrafo.appendChild(testoParagrafo);
+divTitoli.appendChild(paragrafo);
+
+let cardFooter = document.createElement("div");
+cardFooter.className = "card-footer";
+
+let divEtichette = document.createElement("div");
+divEtichette.className = "etichette";
+
+let divSpan1 = document.createElement("span");
+let testoSpan1 = document.createTextNode("Hot");
+divSpan1.appendChild(testoSpan1);
+divEtichette.appendChild(divSpan1);
+
+let divSpan2 = document.createElement("span");
+let testoSpan2 = document.createTextNode("Cold");
+divSpan2.appendChild(testoSpan2);
+divEtichette.appendChild(divSpan2);
+
+cardFooter.appendChild(divEtichette);
+divCardBody.appendChild(cardFooter);
+
+let tasto = document.createElement("button");
+let scrittaTasto = document.createTextNode("Compra");
+
+tasto.appendChild(scrittaTasto);
+cardFooter.appendChild(tasto);
+
+
+
+
+
+
+
+
+
+
+
+
