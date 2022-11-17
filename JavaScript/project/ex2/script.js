338584fd28d2946d78846730a795f53dbb3d24cf
Oleksandr Oleksyuk <oleksandr.oleksyuk97@icloud.com>
2022-11-17 11:36:47 +0100

    finish ex2

--- JavaScript/project/ex2/script.js
+++ JavaScript/project/ex2/script.js
@@ -1,16 +1,35 @@
-// 'use strict';
+'use strict';
 
-const input = document.createElement('input');
-document.body.appendChild(input);
-const btn = document.createElement('button');
-const btnText = document.createTextNode('Aggiungimi');
-btn.appendChild(btnText);
-document.body.appendChild(btn);
+// creazione casella di input
+const inputText = document.createElement('input');
+inputText.setAttribute('type', 'text');
+inputText.id = 'text-value';
+document.body.appendChild(inputText);
+
+// creazione bottone per applicazione stile
+const inputBtn = document.createElement('input');
+inputBtn.setAttribute('type', 'button');
+inputBtn.setAttribute('value', 'Aggiornami');
+document.body.appendChild(inputBtn);
+
+// creazione quadrato colorato
 const div = document.createElement('div');
 document.body.appendChild(div);
-const style = (document.querySelector('div').style =
-  'margin: 2rem; width: 400px; height: 400px; background-color: yellow');
-input.setAttribute('value', style);
-btn.addEventListener('click', function () {
-  console.log('Hello world');
+
+// costante per stile iniziale
+const initialStyle =
+  'margin: 2rem; width: 400px; height: 400px; background-color: yellow';
+
+// applico lo stile iniziale al div
+div.style = initialStyle;
+
+// inserisco lo stile iniziale nella casella di input per orientamento
+inputText.value = initialStyle;
+
+// evento al click sul bottone
+inputBtn.addEventListener('click', function () {
+  // recupero il nuovo stile inserito nella casella di testo
+  let style = inputText.value;
+  // applica il nuovo stile al div
+  div.style = style;
 });
