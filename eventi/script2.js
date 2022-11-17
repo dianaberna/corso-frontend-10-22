72d822788a974fff49ceb84819de5d15ced28378
fabiobillo <billerifabio@gmail.com>
2022-11-17 09:31:19 +0100

    add solution ex n.2 -change on click button event- eventi/script2.js - eventi/index2.html

--- eventi/script2.js
+++ eventi/script2.js
@@ -1,27 +1,9 @@
 window.addEventListener("load", function () {
-   /* let r=Math.floor(Math.random()*256)
-    let g=Math.floor(Math.random()*256)
-    let b=Math.floor(Math.random()*256)
-    let rgb= "rgb("+r+","+g+","+b+")"
-    console.log(rgb)
-    sBody=document.body.style
-    sBody.backgroundColor = rgb
-    sBody.display="flex"
-    sBody.justifyContent="center"
-    sBody.alignItems="center"
-    sBody.alignContent="center"
-    sBody.height="100vh"
-    sBody.width="100vw"
-    let visualizza=document.createElement("div")
-    visualizza.className="colore"
-    sVis=visualizza.style
-    sVis.backgroundColor="white"
-    sVis.color="black"
-    sVis.maxWidth="300px"
-    sVis.fontSize="30px"
-    sVis.padding="15px"
-    let testo=document.createTextNode(rgb)
-    visualizza.appendChild(testo)
-    document.body.appendChild(visualizza) */
-
-  });
+    let StileQuadrato = document.getElementById("quadrato").style;
+    StileQuadrato.cssText ="margin: 2rem; width: 400px; height: 400px; background-color: yellow";
+    document.getElementById("cambio").style.width = "500px";
+    document.getElementById("cambio").value = StileQuadrato.cssText;
+    document.getElementById("aggiornaStile").addEventListener("click", function () {
+            StileQuadrato.cssText = document.getElementById("cambio").value;
+        });
+});
